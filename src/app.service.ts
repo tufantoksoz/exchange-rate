9635c86d31b570c0fef8c26f135def31cab043ae
Tufan <tufantoksozz@gmail.com>
2022-01-03 16:31:38 +0300

    fxp

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import convert = require('xml-js');
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
-import axios from 'axios';
+const {XMLParser, XMLBuilder} = require('fast-xml-parser');
+
+const parser = new XMLParser();
+const builder = new XMLBuilder();
 
 @Injectable()
 export class AppService {
@@ -38,24 +40,12 @@ export class AppService {
 
     const url = `https://www.tcmb.gov.tr/kurlar/${year}${month}/${day}${month}${year}.xml`;
 
-    //---------------------
-    // data tam geliyor ama string olarak geliyor.
-
-    // const { data } = await lastValueFrom(this.httpService.get(url));
-
-    // const jsonData = convert.xml2json(data);
-    // console.log(jsonData);
-    // console.log(typeof jsonData)
-
-    //---------------------
-    // data eksik geliyor ama object olarak geliyor.
 
     const { data } = await lastValueFrom(this.httpService.get(url));
 
-    const jsonData = convert.xml2js(data);
+    const jsonData = parser.parse(data);
+
     console.log(jsonData);
     console.log(typeof jsonData);
-
-    // JSON.parse çalışmıyor. O yüzden xml-js paketini kullandım.
   }
 }
